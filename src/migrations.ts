f84cf40b9b7d943ccdbb982361524ff97e6795e9
pedrohenriqueduarte <ph09duarte@gmail.com>
2022-03-21 01:37:51 -0300

    ajuste no migrations

--- src/migrations.ts
+++ src/migrations.ts
@@ -15,7 +15,7 @@ export const createTables = async () => {
       start_time INT NOT NULL,
       end_time INT NOT NULL,
       band_id VARCHAR(255) NOT NULL,
-      FOREIGN KEY(band_id) REFERENCES NOME_TABELA_BANDAS(id)
+      FOREIGN KEY(band_id) REFERENCES festBandas(id)
     );
 
     CREATE TABLE IF NOT EXISTS festUsers (
